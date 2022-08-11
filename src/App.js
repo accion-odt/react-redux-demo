d28664aec90a783c8376eb89686037e6903eec32
lavanyacheemakurthy <chslavanya@yahoo.com>
2022-08-11 16:45:48 +0530

    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
-import './App.css';
+import "./App.css";
 
 import { Welcome } from "./components/Welcome";
 
-import { useState } from "react";
-import { Session1 } from './components/Session1';
-import Session2 from './components/Session2';
-import {Session3} from './components/Session3';
-import { Session4 } from './components/Session4';
+import { Session1 } from "./components/Session1";
+import Session2 from "./components/Session2";
+import { Session3 } from "./components/Session3";
+import { Session4 } from "./components/Session4";
+import ComponentOne from "./components/ComponentOne";
+import ComponentTwo from "./components/ComponentTwo";
+import ComponentThree from "./components/ComponentThree";
 export const base_url =
   "https://react-demo-97e86-default-rtdb.asia-southeast1.firebasedatabase.app";
 export const paths = {
@@ -16,39 +17,25 @@ export const paths = {
 // environmnetal variables
 function App() {
   
-  const handleAddingNewrecord = () => {
-    fetch(base_url + paths.postUrl + ".json", {
-      method: "POST",
-      body: JSON.stringify({
-        name: "React" + Math.random(),
-        id: 2,
-        address: "us",
-      }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-      });
-  };
-
   return (
     <>
-
       <Welcome />
 
-     <div style={{margin:'20px',padding:'10px'}}>
-     
-      <Session1 />
-      <Session2 />
-      <Session3 />
-      <Session4 />
-      
-      
-
-     </div>
+      <div style={{ margin: "20px", padding: "10px" }}>
+        <Session1 />
+        <Session2 />
+        <Session3 />
+        <Session4 />
+        
+      </div>
+      <div style={{backgroundColor:'blue'}}>Redux example</div>
+      <div   >
+          <ComponentOne />
+          <ComponentTwo />
+          <ComponentThree />
+        </div>
     </>
   );
 }
 
 export default App;
-
